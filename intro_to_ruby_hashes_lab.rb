a3535ddfa7f3426b134f2364098846faf267b6c3
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-05 15:06:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,10 +19,10 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
       },
     :names => {
-      :reading_railroad => {},
-      :pennsylvania_railroad => {},
-      :b_and_o_railroad => {},
-      :shortline_railroad => {}
+      :reading_railroad => {$100},
+      :pennsylvania_railroad => {$100},
+      :b_and_o_railroad => {$100},
+      :shortline_railroad => {$100}
       }
     }
   }
