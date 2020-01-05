867f2b466b28d010e924f02cc5c8da46a00c2835
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-05 14:54:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => :pieces}
+  monopoly = {:railroads => {
+    :pieces => {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
