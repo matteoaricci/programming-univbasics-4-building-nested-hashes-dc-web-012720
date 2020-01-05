77f08ec294cd9ddd11bcf08aa8f616e30621973c
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-05 14:57:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,12 @@ end
 def monopoly_with_third_tier
   monopoly = {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {1, 2, 3, 4}
+    :rent_in_dollars => {
+      :one => {},
+      :two => {},
+      :three => {},
+      :four => {}
+      }
     }
   }
 end
